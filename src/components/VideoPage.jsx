f61e663a5d30129d56e58de7a26b8c9c92ed2f5c
vicky-24-prog <theunknownthought26@gmail.com>
2026-01-07 22:09:17 +0530

    Added feature / fixed bug

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Video from "../assets/WelcomeVideo.mp4";
 import "../styles/videopage.css";
-
+//hi comments here 
 function VideoPage() {
     const navigate = useNavigate();
     const [fade, setFade] = useState(false);
